e78431249a7cede6311ae8b00c387fc3c531ef32
Baklakus <deletedel776@gmail.com>
2025-05-26 02:20:05 +0500

    1

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        from django.contrib.auth.models import Group
+        def create_roles(sender, **kwargs):
+            Group.objects.get_or_create(name='Администратор')
+            Group.objects.get_or_create(name='Ведущий')
+
+        post_migrate.connect(create_roles, sender=self)
