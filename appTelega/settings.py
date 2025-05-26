8995bae513012b89ab707c59ac18547c29237e4c
Baklakus <147690403+Baklakus@users.noreply.github.com>
2025-05-26 06:29:30 +0500

    Update settings.py

--- appTelega/settings.py
+++ appTelega/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Обслуживание статических файлов
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
