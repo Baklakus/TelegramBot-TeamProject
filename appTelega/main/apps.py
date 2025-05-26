0227b81c246959b55c7854126f65bf12ab7a4b16
Baklakus <147690403+Baklakus@users.noreply.github.com>
2025-05-26 06:34:34 +0500

    Update apps.py

--- appTelega/main/apps.py
+++ appTelega/main/apps.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.models import User
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+
+def create_superuser(sender, **kwargs):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(username='admin', password='admin123', email='admin@example.com')
